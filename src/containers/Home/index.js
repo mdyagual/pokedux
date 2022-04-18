e0e8b7b42bbcbf5605d976b51bb5266114578e81
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-04-17 23:37:31 -0500

    Cambio de containers a hooks

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,30 +2,38 @@ import React,{useEffect} from 'react';
 import PokeList from '../../components/PokeList';
 import Searcher from '../../components/Searcher';
 import './styles.css';
-import { getPokemons } from '../../api/getPokemons'
-import { setPokemonAction } from '../../actions/actions';
-import { connect } from 'react-redux';
+//import { getPokemons } from '../../api/getPokemons'
+import { getPokemonDetailedAction, setPokemonAction } from '../../actions/actions';
+import { useDispatch, useSelector } from 'react-redux';
+//import axios from 'axios';
+//import { connect } from 'react-redux';
 
-const mapStateToProps = (state) => ({
+/*const mapStateToProps = (state) => ({
   pokemons: state.list
 });
 
 const mapDispatchToProps = (dispatch) => ({
   setPokemons: value => dispatch(setPokemonAction(value))
 });
+*/
 
+function Home() {
+  const dispatch = useDispatch();
 
-function Home({pokemons,setPokemons}) {
-  useEffect(() => {
-    getPokemons().then(res =>setPokemons(res.results));
-  });
+  const list = useSelector(state => state.list)
+
+  useEffect(() => {   
+      dispatch(getPokemonDetailedAction())
+
+  },[]);
 
   return (
     <div className='Home'>
       <Searcher />
-      <PokeList/>
+      <PokeList pokemons={list}/>
     </div>
   );
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(Home);
+//export default connect(mapStateToProps,mapDispatchToProps)(Home);
+export default (Home);
