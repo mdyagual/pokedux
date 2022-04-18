5a77abc681af4807ffa3fc291a8a3f3338158911
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-04-17 23:36:22 -0500

    Consumo de la API para mostrar la información

--- src/components/PokeList/index.js
+++ src/components/PokeList/index.js
@@ -2,15 +2,24 @@ import React from "react";
 import { Grid } from "semantic-ui-react";
 import PokeCard from './PokeCard';
 import './styles.css';
-const PokeList = () => {
+const PokeList = ({pokemons}) => {
     //const pokemons = [];
-    const pokemons = Array(20).fill('');
+    //const pokemons = Array(20).fill('');
 
     return (
         <Grid className="pokelist">
-        {pokemons.map((p => (<PokeCard/>)))}
+            {
+                pokemons.map((p,id)=>{
+                    return <PokeCard pokemon={p} key={`pokeID-${id}`}/>
+                })
+            
+            };
         </Grid>
     );
+};
+
+PokeList.defaultProps = {
+    pokemons:[]
 }
 
 export default PokeList;
