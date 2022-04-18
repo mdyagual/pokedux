2961c1df8d766712f43e5d12e4e1cd183c6953ab
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-04-17 23:51:37 -0500

    Migración para aplicación de redux-thunk

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { getPokemons } from '../api/getPokemons';
 import * as actions from '../utils/actionTypes';
 
 export const setPokemonAction = (pokemons) => {
@@ -5,4 +7,15 @@ export const setPokemonAction = (pokemons) => {
         type: actions.SET_POKEMOM,
         payload: pokemons
     };
+};
+
+export const getPokemonDetailedAction = () => (dispatch) => {
+   getPokemons().then(res=>{
+       const pokeList = res.results;
+       return Promise.all(pokeList.map(p=>axios.get(p.url)))
+   })
+   .then(pokeResponse => {
+       const pokeInfo = pokeResponse.map(response => response.data);
+       dispatch(setPokemonAction(pokeInfo));
+   });
 };
