bf7c3ae3c90aaf2037098a9a9483764f4410f393
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-04-17 23:35:19 -0500

    Consumo de la API para mostrar la información

--- src/components/PokeList/PokeCard.jsx
+++ src/components/PokeList/PokeCard.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { GridColumn, Icon, Image, Label } from 'semantic-ui-react'
+import pokeReducer from '../../reducers/pokeReducer'
 import { MAIN_COLOR, FAV_COLOR } from '../../utils/constants'
 
-const PokeCard = () => {
+const PokeCard = ({pokemon}) => {
     return <div className='PokeCard'>
         <GridColumn mobile={16} tablet={8} computer={4}>
             <Icon name='favorite' color={FAV_COLOR}/>
-            <Image centered src="" alt="PokeInit"/>
-            <p>PokeNombre</p>
-            <Label color={MAIN_COLOR}>Normal</Label>
+            <Image centered src={pokemon.sprites.front_default} alt="PokeInit"/>
+            <p>{pokemon.name}</p>
+            {pokemon.types.map((t,id) => <Label color={MAIN_COLOR} key={id}>{t.type.name}</Label>)}
         </GridColumn>
     </div>
 }
