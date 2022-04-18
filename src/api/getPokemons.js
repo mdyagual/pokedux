d75c29a81e01d776ae3791744af6e42fa4c2fcb7
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-04-18 01:28:59 -0500

    Consumo del promise en su propio método con Axios

--- src/api/getPokemons.js
+++ src/api/getPokemons.js
@@ -4,3 +4,11 @@ export const getPokemons = (limit = 151) =>
         axiosInstance.get(`/pokemon?limit=${limit}`)
         .then((res)=>res.data)
         .catch((err)=> console.log(err));
+
+export const getPokemonsDetailed = (pokemons) => {
+        return Promise.all(pokemons.map((pokemon) => axiosInstance.get(pokemon.url))).then(
+        (pokemonResponses) => {
+            return pokemonResponses.map((response) => response.data);
+        }
+    );
+};
