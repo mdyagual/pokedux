11f5ff09631686301f666858837c69436ec33fec
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-04-17 23:51:11 -0500

    Agregación de redux-thunk en el middleware

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import App from './containers/App';
 import './index.css';
 import { logActions } from './middleware';
 import pokeReducer from './reducers/pokeReducer';
 
-const composedEnhacers = compose(applyMiddleware(logActions));
+const composedEnhacers = compose(applyMiddleware(thunk,logActions));
 
 const store = createStore(pokeReducer,composedEnhacers);
 
